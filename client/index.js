91b262a50db6f2f2bf816cf681cf3d8a34af9121
Domiii <dominik.seifert@gmail.com>
2019-02-25 23:29:26 +0800

    - added debugging feature for recipes

--- client/index.js
+++ client/index.js
@@ -1,95 +1,96 @@
 const config = {
-      apiKey: "AIzaSyA1W6fgbxENa7AbChzrZuKVRFSiQwBauLg",
-      authDomain: "cproject-9bb5f.firebaseapp.com",
-      databaseURL: "https://cproject-9bb5f.firebaseio.com",
-      projectId: "cproject-9bb5f",
-      storageBucket: "cproject-9bb5f.appspot.com",
-      messagingSenderId: "926906729227"
-  }
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyA1W6fgbxENa7AbChzrZuKVRFSiQwBauLg",
+  authDomain: "cproject-9bb5f.firebaseapp.com",
+  databaseURL: "https://cproject-9bb5f.firebaseio.com",
+  projectId: "cproject-9bb5f",
+  storageBucket: "cproject-9bb5f.appspot.com",
+  messagingSenderId: "926906729227"
+}
+firebase.initializeApp(config);
 
 var btnSignup = document.getElementById('btnSignup');
 var txtEmail = document.getElementById('txtEmail');
 var txtPassword = document.getElementById('txtPassword');
 
 // Registration-btnSignup
-btnSignup.addEventListener('click', function(e){
-    var email = txtEmail.value;
-    var password = txtPassword.value;
-    var auth = firebase.auth();
-    const fName = document.getElementById('fname').value;
-    const lName = document.getElementById('lname').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    //const fs = new firebase.firestore.Firestore;
-    console.log("fName:", fName);
-    console.log("lName:", lName);
-    console.log("phoneNumber:", phoneNumber);
-    auth.createUserWithEmailAndPassword(email, password).then((user) => {
-      localStorage.setItem("currentUserUID", JSON.stringify({uid: user.user.uid})); 
+btnSignup.addEventListener('click', function (e) {
+  var email = txtEmail.value;
+  var password = txtPassword.value;
+  var auth = firebase.auth();
+  const fName = document.getElementById('fname').value;
+  const lName = document.getElementById('lname').value;
+  const phoneNumber = document.getElementById('phoneNumber').value;
+  //const fs = new firebase.firestore.Firestore;
+  console.log("fName:", fName);
+  console.log("lName:", lName);
+  console.log("phoneNumber:", phoneNumber);
+  auth.createUserWithEmailAndPassword(email, password).then((user) => {
+    localStorage.setItem("currentUserUID", JSON.stringify({ uid: user.user.uid })); 
       
-      
+
 
       firebase.firestore().collection("users").add({
-        "email": email || "",
-        "firstname": fName || "",
-        "lastname": lName || "",
-        "phone": phoneNumber || "",
-        "userUID": user.user.uid
-      }).then(() => {
-        console.log("User saved");
-        window.location.href = '/client/profilepage.html';
-      }).catch(console.error);
-      console.log("user:", user);
-    }).catch(console.error);
-});
+      "email": email || "",
+      "firstname": fName || "",
+      "lastname": lName || "",
+      "phone": phoneNumber || "",
+      "userUID": user.user.uid
+      .then(() => {
+      console.log("User saved");
+      window.location.href = '/client/profilepage.html';
+      .catch(console.error);
+    console.log("user:", user);
+    .catch(console.error);
+  
 
 // Login
+// (test login: juginac@eoffice.top/juginac@eoffice.top)
 var btnLogin = document.getElementById('btnLogin');
 var txtLoginEmail = document.getElementById('txtLoginEmail');
 var txtLoginPassword = document.getElementById('txtLoginPassword');
 
 btnLogin.addEventListener('click', function(e){
-  var email = txtLoginEmail.value;
+  var email = txtLoginEmail.value;  
   var password = txtLoginPassword.value;
   var auth = firebase.auth();
   auth.signInWithEmailAndPassword(email, password)
   .then(user => {
-    localStorage.setItem("currentUserUID", JSON.stringify({uid: user.user.uid})); 
-    window.location.href = '/client/profilepage.html';
+      localStorage.setItem("currentUserUID", JSON.stringify({uid: user.user.uid})); 
+      window.location.href = '/client/profilepage.html';  
   })
-  .catch(function(){
-    alert('Wrong Email or Password');
-  });
-})
-
+      atch(function(){
+      alert('Wrong Email or Password');
+    });  
+      
+    
 var btnLogin2 = document.getElementById('btnLogin2');
 var txtLoginEmail2 = document.getElementById('txtLoginEmail2');
 var txtLoginPassword2 = document.getElementById('txtLoginPassword2');
 
 btnLogin2.addEventListener('click', function(e){
   var email = txtLoginEmail2.value;
-  var password = txtLoginPassword2.value;
+  var password = txtLoginPassword2.value;  
   var auth = firebase.auth();
   auth.signInWithEmailAndPassword(email, password)
   .then(user => {
     localStorage.setItem("currentUserUID", JSON.stringify({uid: user.user.uid})); 
-    window.location.href = '/client/profilepage.html';
-  })
+      window.location.href = '/client/profilepage.html';
+        
   .catch(function(){
-    alert('Wrong Email or Password');
-  });
-})
-
-var btnForgotPassword = document.getElementById('btnForgotPassword');
+      alert('Wrong Email or Password');
+    });
+      
+      
+    r btnForgotPassword = document.getElementById('btnForgotPassword');
 btnForgotPassword.addEventListener('click', function(e){
   var auth = firebase.auth();
   auth.sendPasswordResetEmail();
-})
+})  
 
 $('.join').on('click', function(event) {
   console.log('clicked')
   event.preventDefault();
-
+ 
   window.scrollTo(0, $('#register').offset().top - 70);
 
   return false;
@@ -110,12 +111,12 @@ $('.join').on('click', function(event) {
 $('.learn-more').on('click', function(event) {
   // Add class to open the curtain with CSS
   $('.curtain footer').fadeOut(function() {
-    $('body').addClass('loaded');
+    $('body').addClass('loaded'); 
     
-    // Removes the curtain after 1 second
+    // Removes the curtain after 1 seco nd
     setTimeout(function() {
-      $('.curtain').remove();
+  $('.curtain').remove();
     }, 1000);
-  });
+  }); 
 });
 
--- client/profilepage.js
+++ client/profilepage.js
@@ -1,6 +1,6 @@
-console.log("Profile is working");
-
-console.log("firebase:", firebase.firestore)
+// ################################################################################################
+// Setup + Configuration
+// ################################################################################################
 
 const config = {
   apiKey: "AIzaSyA1W6fgbxENa7AbChzrZuKVRFSiQwBauLg",
@@ -12,32 +12,83 @@ const config = {
 }
 firebase.initializeApp(config);
 
-//firebase.auth().onAuthStateChanged(function(user) {
-  const uidObj = JSON.parse(localStorage.getItem("currentUserUID"));
-  if (uidObj.uid) {
+// ################################################################################################
+// Auth + basic user setup
+// ################################################################################################
+
+firebase.auth().onAuthStateChanged(renderUser);
+
+// ################################################################################################
+// Additional user data
+// ################################################################################################
+
+function renderUser(user) {
+  if (user) {
     // User is signed in.
-    console.log("User is logged in with uid:", uidObj);
-    firebase.firestore().doc("users/id").get().then(user => {
-      if(!user.exists) {
-        return;
-      }
-      const userData = user.data();
-    })
+    const { uid, displayName, email } = user;
+    $('#user-name').text(displayName + ' (' + email + ')');
 
-    firebase.firestore().collection("users").limit(4).get().then(users => {
-      if (!users.empty) {
-        const userData = users.docs[0].data();
+    //   firebase.firestore().doc("users/id").get().then(user => {
+    //     if (!user.exists) {
+    //       return;
+    //     }
+    //     const userData = user.data();
+    //   })
 
-        console.log("Got user:", userData);
-        const userDataDiv = document.getElementById('userData');
-        userDataDiv.innerHTML = JSON.stringify(userData.email);
-      }
-    }).catch(err => {
-      console.error(err);
-    });
-  } else {
-    console.error("No user found");
-    // User is signed out.
-    // ...
+    //   firebase.firestore().collection("users").limit(4).get().then(users => {
+    //     if (!users.empty) {
+    //       const userData = users.docs[0].data();
+
+    //       console.log("Got user:", userData);
+    //       const userDataDiv = document.getElementById('userData');
+    //       userDataDiv.innerHTML = JSON.stringify(userData.email);
+    //     }
+    //   }).catch(err => {
+    //     console.error(err);
+    //   });
+    // } else {
+    //   console.error("No user found");
+    //   // User is signed out.
+    //   // ...
   }
-//});
+}
+
+
+
+// ################################################################################################
+// Recipe Debugging
+// ################################################################################################
+
+// "Add recipes" button
+$('#debug-btn-add-recipes').click(async (evt) => {
+  const n = 100;
+
+  // add 100 recipes
+  const promises = [];
+  for (let i = 0; i < n; ++i) {
+    firebase.firestore().collection("recipes").add({
+      title: 'recipe-' + i
+    });
+  }
+
+  const results = await Promise.all(promises);  // wait until all recipes have been added  
+  console.log('added ' + n + ' recipes.');
+});
+
+
+// "all recipes" list
+// see basic example in official documentation: https://firebase.google.com/docs/firestore/query-data/get-data
+const $recipeList = $('#debug-recipe-list');
+$recipeList.text('loading...');
+firebase.firestore().collection("recipes").get().then(function(querySnapshot) {
+  $recipeList.empty(); // clear list
+
+  // add all recipes
+  querySnapshot.forEach(function(doc) {
+      const id = doc.id;
+      const recipe = doc.data();
+      const $recipeEl = $(`<li id="recipe-${id}">${recipe.title}</li>`);
+      $recipeList.append($recipeEl);
+  });
+});
+
